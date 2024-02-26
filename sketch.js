752036a812b94d55fcfe86835ca691f030d40789
abrenzink <a.brenzink@gmail.com>
2024-02-26 09:58:13 -0300

    comments added to all actions

--- sketch.js
+++ sketch.js
@@ -11,20 +11,21 @@ var ground, invisibleGround, groundImg;
 
 // clouds group and images
 var cloudsGroup, cloudImg;
+
 //obstacles group and images
 var obstaclesGroup, obst1, obst2, obst3, obst4, obst5, obst6;
 
 var score = 0;
 
-// game over and restart buttons 
-var gameOver, restart;
+// game over and restartBtn buttons 
+var gameOver, restartBtn;
 
 // sound variables
 var dieSound, jumpSound, checkpointSound;
 
-// here we load ani images and sounds to be used in the game
+// here we load any images and sounds to be used in the game
 function preload(){
-  trex_running =   loadAnimation("assets/trex1.png","assets/trex2.png","assets/trex3.png");
+  trex_running = loadAnimation("assets/trex1.png","assets/trex2.png","assets/trex3.png");
   trex_colision = loadAnimation("assets/trex_collided.png");
   
   groundImg = loadImage("assets/ground2.png");
@@ -47,91 +48,137 @@ function preload(){
 
 }
 
+// here we set the initial states for all elements when function draw is executed for the very first time
 function setup() {
+  // create the canvas (where sprites are displayed)
   createCanvas(600, 200);
-  
-  trex = createSprite(50,180,20,50);
-  
+
+  // create the t-rex object (or sprite) and define an image/animation for it
+  trex = createSprite(50,180,20,50);  
   trex.addAnimation("running", trex_running);
   trex.addAnimation("collided", trex_colision);
   trex.scale = 0.5;
-  
+
+  // create the ground sprite and define img, velocity, size for it
   ground = createSprite(200,180,400,20);
   ground.addImage("ground",groundImg);
   ground.x = ground.width /2;
   ground.velocityX = -(6 + 3*score/100);
   
+
+  // create the game over sprite and define an image for it
   gameOver = createSprite(300,100);
   gameOver.addImage(gameOverImg);
-  
-  restart = createSprite(300,140);
-  restart.addImage(restartImg);
-  
   gameOver.scale = 0.5;
-  restart.scale = 0.5;
 
+  // create the restart button sprite and define the img for it
+  restartBtn = createSprite(300,140);
+  restartBtn.addImage(restartImg);  
+  restartBtn.scale = 0.5;
+
+
+  // both game over and restart button should not be visible at the game start
   gameOver.visible = false;
-  restart.visible = false;
+  restartBtn.visible = false;
   
+
+  // create the invisible ground hold the t-rex
   invisibleGround = createSprite(200,190,400,10);
   invisibleGround.visible = false;
   
+  /* 
+    clouds and obstacles arrays. This will allow us to define 
+    actions to all elements of the array at once
+  */
   cloudsGroup = new Group();
   obstaclesGroup = new Group();
   
+  // defne initial score
   score = 0;
 }
 
+/* 
+  draw is a constantly looping function. This allows sprites to be drawn 
+  and updated on the screen, giving the illusion of movement. 
+  It runs 60 times per second, or 60 FPS (frames per second).
+*/
 function draw() {
 
+  // background color
   background(255);
-  text("Score: "+ score, 500,50);
-  
+  text("Score: "+ score, 500, 50);  
+
+  // define elements' behaviors if we are playing
   if (gameState === PLAY){
+
+    // increase score as the game continues
     score = score + Math.round(getFrameRate()/60);
+    // change ground speed to give illusion of speed
     ground.velocityX = -(6 + 3*score/100);
 
+    // plays the checkpoint sound every 100 points
     if(score != 0 && score % 100 == 0){
       checkpointSound.play();
     }
- 
-    trex.changeAnimation("running", trex_running);
     
-    if(keyDown("space") && trex.y >= 159) {
+    // if space is pressed, t-rex jumps and the jump sound is played
+    if(keyDown("space") && trex.y >= 159) { // the jump should not be active before 159 pixels
       trex.velocityY = -12;
       jumpSound.play();
     }
-  
-    trex.velocityY = trex.velocityY + 0.8
-  
+
+    // this defines gravity
+    trex.velocityY = trex.velocityY + 0.8;
+
+    /*
+      The visible image of the ground is reloaded when it reaches 
+      halfway across the screen to give the illusion of an endless ground
+    */
     if (ground.x < 0){
       ground.x = ground.width/2;
     }
-  
+
+    //define t-rex animation to 'runnning'
+    trex.changeAnimation("running", trex_running);
+
+    /*
+      defines collision between t-rex sprite and invisible ground 
+      sprite to prevent gravity from pulling the t-rex to infinity
+    */
     trex.collide(invisibleGround);
+    // spawn clouds and obstacles
     spawnClouds();
     spawnObstacles();
   
+    // if t-rex touches any sprite in the obstacles array, the game ends
     if(obstaclesGroup.isTouching(trex)){
         gameState = END;
+
+        // play game end sound
         dieSound.play();
     }
   }
+  // define elements' behaviors when the game ends
   else if (gameState === END) {
+    // game over and restart buttons should be visible
     gameOver.visible = true;
-    restart.visible = true;
+    restartBtn.visible = true;
     
+    // ground image stops
     ground.velocityX = 0;
     trex.velocityY = 0;
+    // obstacles and clouds stop
     obstaclesGroup.setVelocityXEach(0);
     cloudsGroup.setVelocityXEach(0);
     
     trex.changeAnimation("collided", trex_colision);
     
+    // obtacles and clouds already on the cavas should be there until the game restarts
     obstaclesGroup.setLifetimeEach(-1);
     cloudsGroup.setLifetimeEach(-1);
     
-    if(mousePressedOver(restart)){
+    // if user clicks restart button, game state changes to PLAY
+    if(mousePressedOver(restartBtn)){
       reset();
     }
   }
@@ -141,66 +188,76 @@ function draw() {
 
 function spawnClouds() {
   
+  // create a new cloud every 60 frames
   if (frameCount % 60 === 0) {
     var cloud = createSprite(600,120,40,10);
+    // create the cloud at a random vertical position between pixel 80 and pixel 120
     cloud.y = Math.round(random(80,120));
     cloud.addImage(cloudImg);
     cloud.scale = 0.5;
     cloud.velocityX = -3;
     
+    // each cloud created will exist during 200 frames
     cloud.lifetime = 200;
-    
+
+    // set the clouds behind t-rex sprite
     cloud.depth = trex.depth;
     trex.depth = trex.depth + 1;
     
+    // add the new cloud to the clouds array
     cloudsGroup.add(cloud);
   }
   
 }
 
 function spawnObstacles() {
+
+  // create new obstacle every 60 frames
   if(frameCount % 60 === 0) {
-    var cactus = createSprite(600,165,10,40);
-    //obstacle.debug = true;
-    cactus.velocityX = -(6 + 3*score/100);
+    var obstacle = createSprite(600,165,10,40);
+    obstacle.velocityX = -(6 + 3*score/100);
     
-    //gere obstáculos aleatórios
+    // set random obstacle image for the obstacle sprite
     var rand = Math.round(random(1,6));
     switch(rand) {
-      case 1: cactus.addImage(obst1);
+      case 1: obstacle.addImage(obst1);
               break;
-      case 2: cactus.addImage(obst2);
+      case 2: obstacle.addImage(obst2);
               break;
-      case 3: cactus.addImage(obst3);
+      case 3: obstacle.addImage(obst3);
               break;
-      case 4: cactus.addImage(obst4);
+      case 4: obstacle.addImage(obst4);
               break;
-      case 5: cactus.addImage(obst5);
+      case 5: obstacle.addImage(obst5);
               break;
-      case 6: cactus.addImage(obst6);
+      case 6: obstacle.addImage(obst6);
               break;
       default: break;
-    }
-    
+    }    
          
-    cactus.scale = 0.5;
-    cactus.lifetime = 300;
+    obstacle.scale = 0.5;
+    // obstacle exists during 300 frames
+    obstacle.lifetime = 300;
     
-    obstaclesGroup.add(cactus);
+    // add obstacle to obstacles array
+    obstaclesGroup.add(obstacle);
   }
 }
 
-
 function reset(){
+  // return to play definitions
   gameState = PLAY;
+  // game over and reset button become invisible again
   gameOver.visible = false;
-  restart.visible = false;
+  restartBtn.visible = false;
 
+  // destroy every cloud or obstacle 
   obstaclesGroup.destroyEach();
   cloudsGroup.destroyEach();
 
   trex.changeAnimation("running", trex_running);
 
+  // reset score to 0
   score = 0;
 }
 
